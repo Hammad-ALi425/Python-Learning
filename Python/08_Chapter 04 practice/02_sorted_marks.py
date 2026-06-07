b8ee7de76e569feb102e85f9328fa415d599314b
Hammad Ali <hammadali00425@gmail.com>
2026-06-08 00:47:43 +0500

    Add files via upload

---
+++ Python/08_Chapter 04 practice/02_sorted_marks.py
@@ -0,0 +1,20 @@
+# Enter marks of 6 student and display in sorted manner
+
+marks = []
+
+m1 = int(input("Enter your marks: "))
+marks.append(m1)                            # It can add the input value in marks
+m2 = int(input("Enter your marks: "))
+marks.append(m2)                            # It can add the input value in marks
+m3 = int(input("Enter your marks: "))
+marks.append(m3)                            # It can add the input value in marks
+m4 = int(input("Enter your marks: "))
+marks.append(m4)                            # It can add the input value in marks
+m5 = int(input("Enter your marks: "))
+marks.append(m5)                            # It can add the input value in marks
+m6 = int(input("Enter your marks: "))
+marks.append(m6)                            # It can add the input value in marks
+
+marks.sort()    # It can sort all marks
+
+print(marks)
