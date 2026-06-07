ebf04739dac444a7fb76d1e4c96eb086ec9a2108
Hammad Ali <hammadali00425@gmail.com>
2026-06-08 00:29:12 +0500

    Add files via upload

---
+++ 03_sum_of_list.py
@@ -0,0 +1,5 @@
+# Sum of list with 4 numbers
+
+my_list = [56,78,54,89]
+sum = sum(my_list)          # It can add the value of the list.
+print(sum)                  # 277
---
+++ 04_count_zeros.py
@@ -0,0 +1,5 @@
+# Count numbers of zeros in tuple
+
+my_tup = (7,0,8,0,0,9)
+print(my_tup.count(0))      # It can count number of zeros and print its count.
+                            # 3
