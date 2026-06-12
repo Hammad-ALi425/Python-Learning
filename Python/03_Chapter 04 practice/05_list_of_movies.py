c20ac74497d5e8d9bf6ddd5abc275fde798b2690
Hammad Ali <hammadali00425@gmail.com>
2026-06-12 06:55:47 +0500

    Delete Python/03_Chapter 04 practice/05_list_of_movies.py

--- Python/03_Chapter 04 practice/05_list_of_movies.py
+++
@@ -1,9 +0,0 @@
-# Ask user to enter 3 movies and store them in a list.
-
-mov1 = input("1st Movie: ")
-mov2 = input("2nd Movie: ")
-mov3 = input("3rd Movie: ")
-
-my_movies = [mov1 , mov2 , mov3]      # It creates a list of Movies.
-print(type(my_movies))                # <class 'list'> 
-print(my_movies)                      # It can print the list of movies that the user enter.
