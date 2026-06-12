e11b79cf3441490ad61418ecb9a5b88f582f1df8
Hammad Ali <hammadali00425@gmail.com>
2026-06-12 06:55:27 +0500

    Delete Python/03_Chapter 04 practice/07_count_grade.py

--- Python/03_Chapter 04 practice/07_count_grade.py
+++
@@ -1,5 +0,0 @@
-# Count number of Students with Grade 'A' in following tuple
-# ("C", "D", "A", "A", "B", "A")
-grade = ("C", "D", "A", "A", "B", "A")
-counted_tuple = grade.count("A")                               # It can count How many times value appear in list.
-print("The Students with Grade 'A' are: " , counted_tuple)     # The Students with Grade 'A' are:  3
